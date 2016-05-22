5ee274602678e13636182a18a5136acec301a995
mousumi12 <mousumi.roy47@gmail.com>
2016-05-23 00:52:27 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
+## The below function will create a matrix object which is invertible, i.e. a non singular matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+    invmatrix <- NULL
+    set <- function(y) {
+        x <<- y
+        invmatrix <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) invmatrix <<- inv
+    getinv <- function() invmatrix
+    list(set = set, get = get,
+    setinv = setinv,
+    getinv = getinv)
 
 }
 
 
 ## Write a short comment describing this function
+## the below function will find the inverse of the matrix denoted by 'x' , if it is already calculated once it will return the inverse from cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invmatrix <- x$getinv()
+        if(!is.null(invmatrix)) {
+            message("getting cached data")
+            return(invmatrix)
+        }
+        data <- x$get()
+        invmatrix <- solve(data, ...)
+        x$setinv(invmatrix)
+        invmatrix
 }
